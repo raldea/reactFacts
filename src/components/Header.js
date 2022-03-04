3ccf09cc67e8889e569d9f08af84fbc59dd61ad9
raldea <59511996+raldea@users.noreply.github.com>
2022-03-04 18:08:25 +0800

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import reactLogo from '../asset/images/reactjs-icon.png';
+
 function Header() {
     return (
         <div className="header">
             <div className="logo-wrapper">
-                <img src="/images/reactjs-icon.png" alt="React Logo"/>
+                <img src={reactLogo} alt="React Logo"/>
                 <div className="title">ReactFacts</div>
             </div>
             <div className="pageHeaderTitle">React Course - Project 1</div>
